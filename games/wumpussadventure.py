3098868fcc34e21938ff4f49f333399dd1f01210
Jake Armstrong <Lainkai@users.noreply.github.com>
2019-06-28 23:37:09 -0400

    I want to do a game, but can't

--- games/wumpussadventure.py
+++ games/wumpussadventure.py
@@ -10,4 +10,4 @@ gLogger.setLevel(logging.DEBUG)
 class wumpussadventure(Game):
         
         def __init__(self, client:discord.Client, ctx):
-            Game.__init__(self, client, ctx, "Wumpus's Adventure!", "Text Adventure","Follow the story of wumpus", 2, 4, True)
+            Game.__init__(self, client, ctx, "Wumpus's Adventure!", "Text Adventure","Follow the story of wumpus", 2, 4, False)
